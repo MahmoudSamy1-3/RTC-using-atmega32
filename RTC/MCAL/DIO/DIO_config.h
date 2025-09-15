2458669fd1728301dee4a36f550d7e028ba9958f
Mahmoud Samy <136725879+MahmoudSamy1-3@users.noreply.github.com>
2025-09-16 00:08:52 +0300

    Add files via upload

---
+++ RTC/MCAL/DIO/DIO_config.h
@@ -0,0 +1,15 @@
+/*
+ * DIO_config.h
+
+
+ *
+ *  Created on: Sep 14, 2025
+ *      Author: Mahmoud Samy, Abdallah Amr, Mohamed Aboelez, Mohamed Karim
+ */
+
+#ifndef DIO_CONFIG_H_
+#define DIO_CONFIG_H_
+
+
+
+#endif /* DIO_CONFIG_H_ */
---
+++ RTC/MCAL/GIE/GLOBINT_interface.h
@@ -0,0 +1,26 @@
+/*
+ * GLOBINT_interface.h
+
+ *
+ *  Created on: Sep 14, 2025
+ *      Author: Mahmoud Samy, Abdallah Amr, Mohamed Aboelez, Mohamed Karim
+ */
+
+#ifndef GLOBINT_INTERFACE_H_
+#define GLOBINT_INTERFACE_H_
+/************************************************************************/
+/************************PUBLIC FUNCTION********************************/
+/**********************************************************************/
+/* DESCRIPTION: Enable Global Interrupt
+ * INPUT      : void
+ * RETUIRN    : void
+ ***************************************************/
+void GLOBINT_voidEnable(void);
+/* DESCRIPTION: Disable Global Interrupt
+ * INPUT      : void
+ * RETUIRN    : void
+ ***************************************************/
+void GLOBINT_voidDisable(void);
+
+
+#endif /* GLOBINT_INTERFACE_H_ */
