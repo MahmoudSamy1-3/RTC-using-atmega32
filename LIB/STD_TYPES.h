ef51a354165a7710bec7325dfdfbbf6e1cc749c1
Mahmoud Samy <136725879+MahmoudSamy1-3@users.noreply.github.com>
2025-09-16 00:04:59 +0300

    Add files via upload

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,25 @@
+/*
+ * STD_TYPES.h
+
+
+ *
+ *  Created on: Sep 14, 2025
+ *      Author: Mahmoud Samy, Abdallah Amr, Mohamed Aboelez, Mohamed Karim
+ */
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char      u8;
+typedef unsigned short int u16;
+typedef unsigned long int  u32;
+
+typedef signed char        s8; 
+typedef signed short int   s16;
+typedef signed long int    s32;
+
+typedef float              f32;
+typedef double             f64;
+
+
+
+#endif
